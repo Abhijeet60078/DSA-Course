07a9f03363ab4723317d0ff15b5fe6c094838089
Abhijeet Mishra <158300568+Abhijeet60078@users.noreply.github.com>
2025-08-24 23:53:13 +0530

    Create Reverse in Linked List.java

---
+++ Reverse in Linked List.java
@@ -0,0 +1,53 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    Node reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+
+    void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.head = new Node(1);
+        list.head.next = new Node(2);
+        list.head.next.next = new Node(3);
+        list.head.next.next.next = new Node(4);
+
+        System.out.println("Original List:");
+        list.printList();
+
+        list.reverse();
+
+        System.out.println("Reversed List:");
+        list.printList();
+    }
+}

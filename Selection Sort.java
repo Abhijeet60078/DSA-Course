7f2a7ba5c073bfaeec037f699b3e8c6a34312f12
Abhijeet Mishra <158300568+Abhijeet60078@users.noreply.github.com>
2025-09-01 23:12:46 +0530

    Create Selection Sort.java

---
+++ Selection Sort.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+import java.util.*;
+
+public class SelectionSort {
+    public static void main(String[] args){
+        int[] arr={13,46,24,52,20,9};
+        System.out.println(Arrays.toString(Selection(arr)));
+
+    }
+    public static int[] Selection(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+        return arr;
+
+
+
+    }
+
+}
